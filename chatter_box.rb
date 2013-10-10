9913eeb93daae266c67ee9e6ea714c5ca34a6dcf
Logan Hasson <logan.hasson@gmail.com>
2013-10-10 14:52:30 -0400

    Initial commit

---
+++ chatter_box.rb
@@ -0,0 +1,41 @@
+class ChatterBox
+
+  COMMANDS = [:hi, :how, :why, :when, :who, :where, :what, :bye]
+
+  def hi
+
+  end
+
+  def how(*args)
+
+  end
+
+  def why(*args)
+
+  end
+
+  def when(*args)
+
+  end
+
+  def who(*args)
+
+  end
+
+  def where(*args)
+
+  end
+
+  def what(*args)
+
+  end
+
+  def bye(*args)
+
+  end
+
+  def call(sentence)
+
+  end
+
+end
